56ee19400886b7fd9ab97aeb272a726f683722c3
Eric Ohwotu <eric.ohwotu@outlook.com>
2017-07-06 09:45:42 -0500

    jenkins test commit 2

--- src/main/scala/com/whiteboardcoder/hello/hw.scala
+++ src/main/scala/com/whiteboardcoder/hello/hw.scala
@@ -2,6 +2,6 @@ package com.whiteboardcoader.hello
 
 object hw{
   def main(args: Array[String]): Unit = {
-    println("Hello World!!!")
+    println("Hello World I love you. you  are the best")
   }
 }
