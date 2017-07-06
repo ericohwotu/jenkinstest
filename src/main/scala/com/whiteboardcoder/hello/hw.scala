e4a1fecaffd2ae89eb3cea1109894667dda1fec5
Eric Ohwotu <eric.ohwotu@outlook.com>
2017-07-06 09:56:51 -0500

    jenkins test commit 3

--- src/main/scala/com/whiteboardcoder/hello/hw.scala
+++ src/main/scala/com/whiteboardcoder/hello/hw.scala
@@ -2,6 +2,10 @@ package com.whiteboardcoader.hello
 
 object hw{
   def main(args: Array[String]): Unit = {
+    val myName = "Eric Ohwotu"
+    val myAge = 25
+   
     println("Hello World I love you. you  are the best")
+    println(s"my name is $myName and i am $myAge years old")
   }
 }
