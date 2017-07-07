efa07f8a453ece38e27324e91c7b5eb245f41a49
Vagrant <vagrant@EricHost>
2017-07-07 04:14:20 -0500

    list with information added

--- src/main/scala/com/whiteboardcoder/hello/hw.scala
+++ src/main/scala/com/whiteboardcoder/hello/hw.scala
@@ -4,8 +4,13 @@ object hw{
   def main(args: Array[String]): Unit = {
     val myName = "Eric Ohwotu"
     val myAge = 25
+    val mList = List("my name","is eric","and i","am great")
    
     println("Hello World I love you. you  are the best")
     println(s"my name is $myName and i am $myAge years old")
+    
+    //hello this is a comment
+    println("this is a message after the comment")
+    println(mList)
   }
 }
